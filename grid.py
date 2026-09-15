9d4252d86e0e5267468f8d5a044ee5897e69fc50
finn2306 <finn.gehret@web.de>
2026-09-15 10:20:57 +0200

    Add Grid class with Cell population via nested loops

--- grid.py
+++ grid.py
@@ -11,3 +11,28 @@ class Cell:
         self.position = position
         self.is_wall = is_wall
         self.weight = weight
+
+class Grid:
+    """Represents a grid of cells.
+    Attributes:
+        width_height (tuple): (w,h) representing the width and height of the grid.
+        cells (tuple): A tuple of Cell objects.
+        def get_walkable_neighbors: get walkable neighbors of current cell.
+    """
+    def __init__(self, width_height: tuple=(25, 25)):
+        self.width_height = width_height
+        self.cells = self.get_cells(width_height)
+
+
+    def get_cells(self, width_height):
+        """Returns a tuple of Cell objects."""
+        cells = []
+        width, height = width_height
+        for w in range(0, width):
+            for h in range(0, height):
+                cell = Cell((w, h))
+                cells.append(cell)
+
+        cells_as_tuple = tuple(cells)
+
+        return cells_as_tuple
